4d084fe685acb5ac40700181d40c506d9627594e
Ainsley McGrath <mcgrath.ainsley@gmail.com>
2019-08-07 13:09:34 -0500

    [master] satisfactory ORM walkthrough

--- examples/querying/orm_style.py
+++ examples/querying/orm_style.py
@@ -1,20 +1,77 @@
+from sqlalchemy.orm import sessionmaker
+
+from examples.source_data import baker_form, all_existing_breads
 from examples.schemas_and_ddl.orm_style import (
-    Bakery,
     Baker,
-    bakery_employees,
     Bread,
     baker_specialty,
     ContactInfo,
 )
 
 
-def insert_update_delete():
-    pass
+def insert_and_update(session):
+    bakers_with_contact_info = [
+        Baker(
+            name=name,
+            pronouns=info_dict["pronouns"],
+            contact=ContactInfo(**info_dict["contact"]),
+        )
+        for name, info_dict in baker_form.items()
+    ]
+
+    session.add_all(bakers_with_contact_info)
+
+    breakpoint()
+    # observe that 8, rather than 4 objects are in session
+    # herein lie the niceties of ORM
+
+    breads = [Bread(**bread_info_dict) for bread_info_dict in all_existing_breads]
+
+    session.add_all(breads)
+
+    breakpoint()
+    # observe how we can query the session even though nothing is committed
+
+    for baker in session.query(Baker).all():
+        specialties_from_form = baker_form[baker.name]["specialties"]
+
+        # updates are asssignments in ORM world
+        baker.specialties = (
+            session.query(Bread).filter(Bread.name.in_(specialties_from_form)).all()
+        )
+
+    breakpoint()
+    # observe the dirty session, note that we didn't touch the bridge table
+
+    session.commit()
 
 
-def selecting_and_joining():
-    pass
+def select_join_delete(session):
+    mediocre_bakers = (
+        session.query(Baker)
+        .join(baker_specialty)
+        .join(Bread)
+        .filter(Bread.is_delicious == False)
+        .all()
+    )
+
+    breakpoint()
+
+    for baker in mediocre_bakers:
+        session.delete(baker)
+
+    breakpoint()
+    # observe, we can rollback! we won't thogugh.
+
+    session.commit()
 
 
-def walkthrough():
-    pass
+def walkthrough(engine):
+    Session = sessionmaker(bind=engine)
+    conn = engine.connect()
+    session = Session(bind=conn)
+
+    insert_and_update(session)
+    select_join_delete(session)
+
+    session.close()
